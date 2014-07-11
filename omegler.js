8fe57197e0ba6200a5f2e5a4277c98f2adb342bd
Dean Sofer <proloser@hotmail.com>
2014-07-11 00:30:38 -0700

    Initial Commit

---
+++ omegler.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+ 
+    var paused = false;
+    var pause = $("<button>Pause Omegler</button>").bind('click', function(){
+        paused = !paused;
+        $(this).text(paused && 'Omegler Paused' || 'Pause Omegler');
+    });
+    var greeting = $("<button>Change Greeting</button>").bind('click', function(){
+        chrome.storage.sync.set({"text": prompt('What would you like your greeting to be?')});
+    });
+    $("#tagline").html('<br>').prepend(pause).append(greeting);
+
+    $("#textbtn").click(function start(){
+        console.log("[debug] start");
+        chrome.storage.sync.get("text", function (val) {
+            write(val["text"],true);
+        });
+    });
+
+    function write(msg,start) {
+        $(".chatmsg", document).html(msg);
+        if ($(".sendbtn", document).is(":disabled")) {
+            return setTimeout(function () {
+                write(msg);
+            }, 200);
+        }
+        stopper = 0;
+        $(".sendbtn", document).click();
+    }
+
+    function reconnect(double){
+        stopper = 1;
+        msgCount = 0;
+        clearInterval(timeout);
+        $(".disconnectbtn", document).click();
+        if(double){
+            $(".disconnectbtn", document).click();
+            $(".disconnectbtn", document).click();
+        }
+        chrome.storage.sync.get("text", function (val) {
+            write(val["text"]);
+        });
+    }
+
+    var msgCount = 0;
+    var stopper = 0;
+    var timeout = 0;
+
+    document.addEventListener("DOMSubtreeModified", function (event) {
+
+        if ($('.strangermsg').length > msgCount) {
+            console.log("[debug] countmessage");
+            msgCount = $('.strangermsg').length;
+        }
+        if(stopper == 0 && $(".newchatbtnwrapper").is(":visible")){
+            console.log("[debug] reconnect button show up");
+            if (!paused)
+                reconnect();
+        }
+
+    });
+
+});
